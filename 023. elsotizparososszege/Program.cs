134a73f33f64a616999cf006459244eb6b0951be
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 16:19:41 +0200

    Az első 10 páros szám összege

---
+++ 023. elsotizparososszege/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _023.elsotizparososszege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szamlalo = 0;
+            int osszeg = 0;
+
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    szamlalo = szamlalo + 1;
+                    osszeg = osszeg + i;
+
+                    if (szamlalo > 10)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(osszeg);
+
+            Console.ReadKey();
+        }
+    }
+}
