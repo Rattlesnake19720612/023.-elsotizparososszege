b660eb6ca4bf17136f4b9c148b8cd741e4d8bc47
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-16 00:20:18 +0200

    Az első 10 páros szám összege

--- 023. elsotizparososszege/Program.cs
+++ 023. elsotizparososszege/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,3 +33,17 @@ namespace _023.elsotizparososszege
         }
     }
 }
+
+/*kiszámítja az első tíz páros szám összegét. Az alábbiakban röviden összefoglalom, hogy a program mit csinál:
+
+A program elindul, és a "using" utasításokkal importálja a szükséges könyvtárakat.
+
+Definiáljuk a "Program" osztályt, amely tartalmazza a "Main" metódust, ami a program belépési pontja.
+
+A "Main" metóduson belül két változót definiálunk: "szamlalo" és "osszeg". "szamlalo" az eddig feldolgozott páros számokat számolja, míg "osszeg" az eddig feldolgozott páros számok összegét tárolja.
+
+A program egy ciklust indít, amely 0-tól 100-ig megy végig. Minden egyes ciklusban ellenőrizzük, hogy az aktuális szám páros-e. Ha igen, akkor növeljük a "szamlalo" változót eggyel, hozzáadjuk az aktuális számot az "osszeg" változóhoz, majd ellenőrizzük, hogy az első tíz páros számot már feldolgoztuk-e. Ha igen, akkor kilépünk a ciklusból a "break" utasítással.
+
+Végül a program kiírja az "osszeg" változó értékét a konzolra, majd várakozik, amíg a felhasználó bármilyen billentyűt meg nem nyom a "Console.ReadKey()" metódus segítségével.
+
+Összességében ez a program egy egyszerű példa arra, hogy hogyan lehet ciklust használni C# nyelven, és hogyan lehet összeadni számokat egy változóban a ciklus futása közben.*/
